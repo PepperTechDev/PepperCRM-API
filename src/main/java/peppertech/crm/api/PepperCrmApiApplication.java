517a16dce64cb764dd6c43d13b250f652aca7df8
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2025-03-11 23:10:14 -0500

    Configure environment variables, custom banner, and migrate to YAML
    Set up a custom banner.txt file to personalize the console output upon application startup. Configured environment variables using a .env file for secure storage of configuration values, and created a .env.example template for reference. Integrated the Dotenv library to load environment variables into the application at runtime. Additionally, migrated the application configuration from application.properties to application.yml for better structure and readability. Updated the main class to load the environment variables with Dotenv on startup, ensuring smooth configuration management.

--- src/main/java/peppertech/crm/api/PepperCrmApiApplication.java
+++ src/main/java/peppertech/crm/api/PepperCrmApiApplication.java
@@ -7,7 +7,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PepperCrmApiApplication {
 
     public static void main(String[] args) {
+        loadEnv();
         SpringApplication.run(PepperCrmApiApplication.class, args);
     }
 
+    private static void loadEnv() {
+        try {
+            Dotenv dotenv = Dotenv.load();
+
+            dotenv.entries().forEach(entry -> {
+                String key = entry.getKey();
+                String value = entry.getValue();
+
+                if (value != null) {
+                    System.setProperty(key, value);
+                }
+            });
+        } catch (Exception e) {
+            log.warn("No se pudo cargar el archivo .env. Se continuará con las variables de entorno ya presentes.");
+            log.debug("Detalles del error: ", e);
+        }
+    }
 }
