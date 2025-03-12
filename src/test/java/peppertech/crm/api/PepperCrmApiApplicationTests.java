5b698b3a38470a3c4b58bb3ad0af4adae7141155
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2025-03-11 22:56:51 -0500

    Initialize Spring Boot project with Java 17
    Initialized a new Spring Boot project using Java 17 and Spring Boot version 3.4.3. The project structure has been set up with the default configurations.

---
+++ src/test/java/peppertech/crm/api/PepperCrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package peppertech.crm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepperCrmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
