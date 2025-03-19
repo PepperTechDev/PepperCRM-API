aa2050abdb7d5a8d61ce34903b12cf62dd7793bf
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2025-03-19 00:16:22 -0500

    feat(auth): add custom exception for token generation errors
    Create a custom exception to handle token generation errors, improving error management and API response consistency

---
+++ src/main/java/peppertech/crm/api/Exceptions/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package peppertech.crm.api.Exceptions;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
