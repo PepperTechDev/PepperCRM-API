824ad6e2553e5a1c6d163f6cefca8a053aa9c2f8
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2025-03-18 23:55:51 -0500

    feat(swagger-config): add custom Swagger configuration
    Define a customized Swagger setup considering debugging levels, default parameters, rendering type, and execution path

---
+++ src/main/java/peppertech/crm/api/Documentation/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package peppertech.crm.api.Documentation;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${TITLE}")
+    private String title;
+
+    @Value("${VERSION}")
+    private String version;
+
+    @Value("${AUTHOR}")
+    private String author;
+
+    @Value("${DESCRIPTION}")
+    private String description;
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .version("v" + version)
+                        .description(description)
+                        .termsOfService("https://github.com/PepperTech/PepperCRM-API/blob/main/LICENSE")
+                        .license(new License().name("MIT License").url("https://opensource.org/license/mit"))
+                        .contact(new Contact().name(author).url("https://github.com/PepperTech").email("peppertech@gmail.com"))
+                );
+    }
+}
