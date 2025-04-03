4e9ad437baf69e63c4ec12a9789ea795f6908f4e
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2025-04-02 22:46:57 -0500

    feat: Implement Telegram Message Sending Service
    Implements a service to handle message sending via Telegram. Introduces the MessageDetails model to structure message data. Adds MessageService to manage the sending logic and MessageServiceI as an interface for modularity. Creates MessageController to expose the service through endpoints. Implements the asynchronous method public void sendMessage(String chatId, String message) to improve responsiveness and efficiency

---
+++ src/main/java/peppertech/crm/api/Messages/Service/MessageService.java
@@ -0,0 +1,53 @@
+package peppertech.crm.api.Messages.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import peppertech.crm.api.Messages.Model.MessageDetails;
+
+@Slf4j
+@Service
+public class MessageService implements MessageServiceI{
+    @Value("${spring.telegram.bot.url}")
+    private String telegramApiUrl;
+
+    private final RestTemplate restTemplate;
+
+    public MessageService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    /**
+     * Enviar un mensaje a un chat específico de Telegram.
+     *
+     * @param chatId  ID del chat o usuario de Telegram. #1515431025
+     * @param message Contenido del mensaje a enviar.
+     */
+    @Async
+    public void sendMessage(String chatId, String message) {
+        try {
+            String url = telegramApiUrl + "sendMessage";
+            MessageDetails telegramMessage = new MessageDetails(chatId, message);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Content-Type", "application/json");
+
+            HttpEntity<MessageDetails> request = new HttpEntity<>(telegramMessage, headers);
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                log.info("Mensaje enviado correctamente a Telegram.");
+            } else {
+                log.warn("Error al enviar mensaje a Telegram: " + response.getBody());
+            }
+        } catch (Exception e) {
+            log.error("Error al enviar mensaje a Telegram", e);
+        }
+    }
+}
---
+++ src/main/java/peppertech/crm/api/Messages/Service/MessageServiceI.java
@@ -0,0 +1,5 @@
+package peppertech.crm.api.Messages.Service;
+
+public interface MessageServiceI {
+   void sendMessage(String chatId, String message);
+}
